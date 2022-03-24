5b19c0a4d9732f9121dc02e04d4b975732e08c95
Aditya Raj Shrivastava <aditya.raj1305@gmail.com>
2022-03-23 22:06:32 -0500

    Project structure check-in

---
+++ src/main/java/com/rsproject/articlemanagementapi/ArticleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.rsproject.articlemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsproject/articlemanagementapi/ArticleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsproject.articlemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
